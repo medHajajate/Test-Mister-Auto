c1b2c8c9b9efde35d856438f978e3f0443042ef3
medhajajate <medhajajate@gmail.com>
2020-09-19 15:35:02 +0100

    init the project

---
+++ Test Mister Auto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Mister Auto
+//
+//  Created by Mohammed Hajajate on 9/19/20.
+//  Copyright © 2020 Mohammed Hajajate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
